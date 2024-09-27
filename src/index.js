a0cf1c08c4886dbd383afcf77dc6af6b13c019af
yangzi_123 <3339524596@qq.com>
2024-09-27 14:48:15 +0800

    登录-使用redux维护token数据

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import { Provider } from 'react-redux'
+import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <RouterProvider router={router}>
-
-    </RouterProvider>
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 )
