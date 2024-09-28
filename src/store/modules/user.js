d0191a55f7e823d5084eeb7ff8fe4ff4aa67924f
yangzi_123 <3339524596@qq.com>
2024-09-28 18:55:34 +0800

    封装apis模块

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,6 +2,7 @@
 
 import { createSlice } from "@reduxjs/toolkit"
 import { setToken as _setToken, getToken, removeToken, request } from "@/utils"
+import { getProfileAPI, loginAPI } from "@/apis/user"
 
 const userStore = createSlice({
   name: 'user',
@@ -39,7 +40,7 @@ const userReducer = userStore.reducer
 const fetchLogin = (loginForm) => {
   return async(dispatch) => {
     // 1.发送异步请求
-    const res = await request.post('/authorizations', loginForm)
+    const res = await loginAPI(loginForm)
     // 2.提交同步action进行token的存入
     dispatch(setToken(res.data.token))
   }
@@ -48,7 +49,7 @@ const fetchLogin = (loginForm) => {
 // 获取个人用户信息异步方法
 const fetchUserInfo = () => {
   return async (dispatch) => {
-    const res = await request.get('/user/profile')
+    const res = await getProfileAPI()
     dispatch(setUserInfo(res.data))
   }
 }
