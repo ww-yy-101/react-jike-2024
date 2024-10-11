93249120770e798b3e3d2edbf74d3153fbe09a35
yangzi_123 <3339524596@qq.com>
2024-10-11 18:07:34 +0800

    打包路由懒加载优化

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,17 @@
 import { createBrowserRouter } from "react-router-dom"
 import Layout from "@/pages/Layout"
 import Login from "@/pages/Login"
-import Home from "@/pages/Home"
-import Article from "@/pages/Article"
-import Publish from "@/pages/Publish"
+// import Home from "@/pages/Home"
+// import Article from "@/pages/Article"
+// import Publish from "@/pages/Publish"
 import { AuthRoute } from "@/components/AuthRoute"
+import { lazy, Suspense } from "react"
 
+// 1.lazy函数对组件进行导入
+
+const Home = lazy(() => import('@/pages/Home'))
+const Article = lazy(() => import('@/pages/Article'))
+const Publish = lazy(() => import('@/pages/Publish'))
 // 配置路由实例
 
 const router = createBrowserRouter([
@@ -17,15 +23,15 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />
+        element: <Suspense fallback={'加载中'}><Home /></Suspense>
       },
       {
         path: 'article',
-        element: <Article />
+        element: <Suspense fallback={'加载中'}><Article /></Suspense>
       },
       {
         path: 'publish',
-        element: <Publish />
+        element: <Suspense fallback={'加载中'}><Publish /></Suspense>
       }
     ]
   },
